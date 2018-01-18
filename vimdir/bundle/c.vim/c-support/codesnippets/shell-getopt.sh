7cc412172e9f4ecb4e91d15616d3265d4b59f08a
XiaoQixue <xiaoqixue_1@163.com>
2018-01-18 19:41:09 +0800

    update shell code getopt in codesnipts

--- vimdir/bundle/c.vim/c-support/codesnippets/shell-getopt.sh
+++ vimdir/bundle/c.vim/c-support/codesnippets/shell-getopt.sh
@@ -2,12 +2,17 @@
 declare -a ARGS
 ARGS=($@)
 
-while getopts "c:bs:@ED" opt ${ARGS[@]} ; do
+while getopts "c:bs:@ED-:" opt ${ARGS[@]} ; do
     case "$opt" in
         c) COMM="$OPTARG" ;;
         b) ENUMDFL=0 ;;
         s) START="$OPTARG" ;;
         @) ;;
+        -) LONG_OPTARG="${OPTARG#*=}"
+            case $OPTARG in
+                alpha ) echo $LONG_OPTARG ;;
+                belt ) echo $LONG_OPTARG ;;
+            esac ;;
         \?) usage ; exit 1 ;;
         *) break ;;
     esac
